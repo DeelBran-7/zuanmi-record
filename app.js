c3ecc39789f2830a1d07a59bfb4297a5a34cb357
Zuanmi User <zuanmi-record@users.noreply.github.com>
2026-06-05 15:13:45 +0800

    Polish fintech visual design

--- app.js
+++ app.js
@@ -1233,7 +1233,7 @@ function escapeAttr(value) {
 
 function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js?v=21').then((registration) => {
+    navigator.serviceWorker.register('./sw.js?v=22').then((registration) => {
       registration.update().catch(() => {});
     }).catch(() => {});
   }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zuanmi-record-v21';
+const CACHE_NAME = 'zuanmi-record-v22';
 const ASSETS = [
   './',
   './index.html',
